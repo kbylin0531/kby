c90f75af4657882b939ea79089d2ad76dc531324
kbylin <kbylin@163.com>
2016-04-29 21:25:47 +0800

    firstday

---
+++ Resource/values.php
@@ -0,0 +1,12 @@
+<?php
+/**
+ * Created by PhpStorm.
+ * User: kbylin
+ * Date: 4/29/16
+ * Time: 6:31 PM
+ */
+return [
+
+
+
+];
--- htdocs/admin.php
+++ htdocs/admin.php
@@ -1,8 +1,11 @@
 <?php
 /**
- * Created by Linzhv.
- * User: Zhonghuang
- * Date: 2016/4/14
- * Time: 14:17
+ * User: linzh
+ * Date: 2016/3/14
+ * Time: 20:50
  */
-include '../index.php';
+const PAGE_TRACE_ON = true;
+const DEBUG_MODE_ON = true;
+
+include '../System/Kbylin.class.php';
+(new Kbylin())->liten(false)->inspect(false)->start();
